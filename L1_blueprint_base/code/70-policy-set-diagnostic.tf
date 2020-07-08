b3f9fcf798b5f421858462ee84889efaee69a352
Bernard Maltais <bmaltais@gmail.com>
2020-07-08 01:46:46 +0000

    Updating gorover.sh Testing using management group level policy initiative

--- L1_blueprint_base/code/70-policy-set-diagnostic.tf
+++ L1_blueprint_base/code/70-policy-set-diagnostic.tf
@@ -1,7 +1,43 @@
 module Project-Diagnostic-Policy {
-  source                  = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-diagnostic_policy_set?ref=v0.1.7"
+  source                  = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-diagnostic_policy_set?ref=v0.1.8"
   deploy                  = var.deployOptionalFeatures.diagnostics_policy
   env                     = var.env
   userDefinedString       = local.prefix
   log_analytics_workspace = local.Project-law
+}
+
+module Management-Diagnostic-Policy {
+  source                  = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-diagnostic_policy_set?ref=v0.1.8"
+  deploy                  = var.deployOptionalFeatures.diagnostics_policy
+  env                     = var.env
+  userDefinedString       = local.prefix
+  log_analytics_workspace = local.Project-law
+  management_group_name = "PBMM_VDC"
+}
+
+locals {
+  policy_set_name = substr("${var.env}-${local.prefix} diagnostic policy set", 0, 64)
+}
+
+resource "azurerm_policy_assignment" "policy_assignment" {
+  count                = var.deployOptionalFeatures.diagnostics_policy ? 1 : 0
+  name                 = local.policy_set_name
+  location             = local.Project-law.location
+  scope                = data.azurerm_subscription.primary.id
+  policy_definition_id = module.Management-Diagnostic-Policy.policy_set_definition.id
+  display_name         = local.policy_set_name
+  description          = "Apply diagnostic settings on subscription for Azure for PBMM Guardrails compliance"
+  identity {
+    type = "SystemAssigned"
+  }
+  parameters = <<PARAMETERS
+  {
+    "logAnalytics": {
+      "value": "${local.Project-law.id}"
+    },
+    "prefix": {
+      "value": "${local.Project-law.name}-"
+    }
+  }
+PARAMETERS
 }
