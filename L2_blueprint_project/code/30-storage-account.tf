fd8fa5018515de255f90dfba11431264a6082f77
bernardmaltais <bernard.maltais@canada.ca>
2020-10-01 16:30:08 -0400

    Update 30-storage-account.tf

--- L2_blueprint_project/code/30-storage-account.tf
+++ L2_blueprint_project/code/30-storage-account.tf
@@ -1,5 +1,5 @@
 module "Project-sa" {
-  source                   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-storage_account?ref=v1.0.1"
+  source                   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-storage_account?ref=v1.0.3"
   env                      = var.env
   userDefinedString        = "main${var.project}"
   resource_group           = local.resource_groups_L2.Project
